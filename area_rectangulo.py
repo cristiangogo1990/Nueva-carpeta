e41d19d6324d35d6b155d37d0504517a3786f194
Cristian_Gómez <cristiangogo1990@gmail.com>
2025-11-08 01:31:51 -0300

    calculo del area de un rectangulo

---
+++ area_rectangulo.py
@@ -0,0 +1,8 @@
+print("CÁLCULO DE AREA DE UN RECTÁNGULO CUALQUIERA")
+
+base=float(input("ingrese un valor para la base del rectángulo: "))
+altura=float(input("ingrese un valor para la altura del rectángulo: "))
+
+area=base*altura
+
+print(f"el área del rectángulo resultante es de: {area:.2f}")
