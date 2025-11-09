4a977b332b56f1388950809a3d478a43bb401c84
Cristian_Gómez <cristiangogo1990@gmail.com>
2025-11-09 13:58:15 -0300

    final_Media_Mediana

--- media_mediana.py
+++ media_mediana.py
@@ -1,34 +1,40 @@
-print("ingrese una serie de datos numericos para calcular su media y mediana")
-print("cantidad de numeros a ingresar: ")
-n=int(input("numero")) # creo un contador
+print("INGRESE UNA SERIE DE DATOS NUMERICOS PARA CALCULAR SU MEDIA Y SU MEDIANA: ")
+print("CANTIDAD DE NUMEROS A INGRESAR: ")
+n=int(input("NUMERO: ")) # creo un contador
 lista=[] #declarar la lista vacia fuera del bucle while
 suma=0
 n2=n
+pos=1
 while n>0:
     
    #creo variable numeros. declaro el tipo entero y se lo carga el usuario por consola
-    numeros=(int(input("ingrese el numero: "))) 
+    print("INGRESE UN NUMERO PARA LA POSICION: ",pos)
+    numeros=(int(input("")))
     lista.append(numeros) #agrego el numero cargado a la variable lista. el vector/arreglo lista
     #calcular Media = (Suma de todos los valores) / (Número total de valores)
-    print("calculo de media: ")
+
     suma=numeros+suma
-    
+    pos=pos+1
     n-=1  #corta el bucle de carga de datos
+print ("LA LISTA CARGADA ES: ",lista) #imprimo string pegado a lista. la coma concatene datos    
+
+print("CALCULANDO LA MEDIA: ")
 media=suma/n2  #sacar la media del bucle
-print ("la lista cargada es: ",lista) #imprimo string pegado a lista. la coma concatene datos
-print ("la media es: ",media)
+
+print ("MEDIA = ",media)
 
 #calcular mediana (ordenar datos)
+print("CALCULANDO MEDIANA: ")
 lista.sort()
-print(lista)
-if n2%2==0:
-    par=n2//2
-    print(lista[par])
+print("DATOS ORDENADOS DE MENOR A MAYOR: ",lista)
+if n2%2!=0:
+    impar=n2//2
+    print("MEDIANA = ", lista[impar])
 else:
-    impar1=(n2//2) #posicion central
-    impar2=(n2//2)+1 #posicion central +1
-    impar=(lista[impar1]+lista[impar2])/2 #promedio de los dos valores tomados de la lista
-    print(lista[impar])
+    par1=(n2//2) #posicion central
+    par2=(n2//2)-1 #posicion central +1
+    par=(lista[par1]+lista[par2])/2 #promedio de los dos valores tomados de la lista
+    print("MEDIANA = ", par)
 #calcular mediana (odenar datos)
 #lista.sort()
 #print(lista)
